b06b1238cd28ab4c6b211512e3c50c6551c1e5d1
Mohamed Waked <mwaked01@gmail.com>
2023-12-25 02:18:46 -0500

    add user authentication with login, signup, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-
-  # namespace :admin do
-  #   get 'categories/index'
-  #   get 'categories/new'
-  # end
   root to: 'products#index'
  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
